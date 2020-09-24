2c523aa91635547fca3198fe4bf31a5015585162
Ivan Matantsev <ivmatan@microsoft.com>
2020-09-24 00:35:16 -0700

    Add KthElement, Swap, Partition as extensions.

---
+++ src/System.Collections.Algorithms/System.Collection.Algorithms.Tests/ExtensionTests.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections.Generic;
+using System.Collections.Algorithms;
+using Xunit;
+
+namespace System.Collection.Algorithms.Tests
+{
+    public class ExtensionTests
+    {
+        [Fact]
+        public void GivenArrayWhenPartitionByElementThenAllElementSmallersThanElementInLeftPart()
+        {
+            int[] data = new int[] { 4, 1, 8, 5, 3, 9, 6 };
+            var element = 9;
+            var pos = data.Partition(0, data.Length, element);
+            int l = 0;
+            while (data[l] < element)
+                l++;
+            Assert.Equal(l, pos);
+            Assert.Equal(element, data[l]);
+            l++;
+            while (l < data.Length)
+            {
+                Assert.True(data[l] >= element);
+                l++;
+            }
+        }
+
+        [Fact]
+        public void GivenListWhenPartitionByElementThenAllElementSmallersThanElementInLeftPart()
+        {
+            var data = new List<int>() { 4, 1, 8, 5, 3, 9, 6 };
+            var element = 6;
+            var pos = data.Partition(0, data.Count, element);
+            int l = 0;
+            while (data[l] < element)
+                l++;
+            Assert.Equal(l, pos);
+            Assert.Equal(element, data[l]);
+            l++;
+            while (l < data.Count)
+            {
+                Assert.True(data[l] >= element);
+                l++;
+            }
+        }
+
+        [Fact]
+        public void GivenArrayWithDuplicateWhenPartitionByDuplicateElementThenAllElementsSmallerThanElementInLeftPart()
+        {
+            int[] data = new int[] { 4, 1, 8, 4, 3, 9, 6 };
+            var element = 4;
+            var pos = data.Partition(0, data.Length, element);
+            int l = 0;
+            while (data[l] < element)
+                l++;
+            Assert.Equal(l, pos);
+            Assert.Equal(element, data[l]);
+            l++;
+            while (l < data.Length)
+            {
+                Assert.True(data[l] >= element);
+                l++;
+            }
+        }
+
+        [Fact]
+        public void GivenArrayAndReverseComparerWhenPartitionByElementThenAllElementsSmallerThanElementInRightPart()
+        {
+            var comparer = Comparer<int>.Create((x, y) => y.CompareTo(x));
+            int[] data = new int[] { 4, 1, 8, 5, 3, 9, 6 };
+            var element = 6;
+            var pos = data.Partition(0, data.Length, element, comparer);
+            int r = data.Length - 1;
+            while (data[r] < element)
+                r--;
+            Assert.Equal(r, pos);
+            Assert.Equal(element, data[r]);
+            r--;
+            while (r > 0)
+            {
+                Assert.True(data[r] >= element);
+                r--;
+            }
+        }
+
+        [Fact]
+        public void GivenArrayWithDuplicateAndReverseComparerWhenPartitionByDuplicateElementThenAllElementsSmallerThanElementInRightPart()
+        {
+            var comparer = Comparer<int>.Create((x, y) => y.CompareTo(x));
+            int[] data = new int[] { 4, 1, 8, 4, 3, 9, 6 };
+            var element = 6;
+            var pos = data.Partition(0, data.Length, element, comparer);
+            int r = data.Length - 1;
+            while (data[r] < element)
+                r--;
+            Assert.Equal(r, pos);
+            Assert.Equal(element, data[r]);
+            r--;
+            while (r > 0)
+            {
+                Assert.True(data[r] >= element);
+                r--;
+            }
+        }
+
+        [Fact]
+        public void GivenArrayWhenFindKthElementThenSameAsKthElementInSortedArray()
+        {
+            var data = new int[] { 4, 1, 8, 4, 3, 9, 6 };
+            var sortedData = new int[data.Length];
+            Array.Copy(data, sortedData, data.Length);
+            Array.Sort(sortedData);
+            for (int i = 0; i < data.Length; i++)
+            {
+                var d = data.KthElement(i);
+                Assert.Equal(sortedData[i], d);
+            }
+        }
+
+        [Fact]
+        public void GivenArrayWehnFindKthElementThenArrayDidntChanged()
+        {
+            var data = new int[] { 4, 1, 8, 4, 3, 9, 6 };
+            var deepCopy = new int[data.Length];
+            Array.Copy(data, deepCopy, data.Length);
+            data.KthElement(6);
+            Assert.Equal(data, deepCopy);
+        }
+
+        [Fact]
+        public void GivenMultipleRandomDataWhenFindKthElementThenSameAsKthElementInSortedArray()
+        {
+            var rand = new Random();
+            var n = 100;
+            var loops = 1000;
+            var data = new int[n];
+            var sortedData = new int[n];
+            for (int loop = 0; loop < loops; loop++)
+            {
+                for (int i = 0; i < n; i++)
+                    data[i] = rand.Next();
+                Array.Copy(data, sortedData, data.Length);
+                Array.Sort(sortedData);
+                for (int i = 0; i < data.Length; i++)
+                {
+                    var d = data.KthElement(i);
+                    Assert.Equal(sortedData[i], d);
+                }
+            }
+        }
+    }
+}
---
+++ src/System.Collections.Algorithms/System.Collections.Algorithms/Extensions.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace System.Collections.Algorithms
+{
+    public static class Extensions
+    {
+        public static void Swap<T>(this IList<T> array, int left, int right)
+        {
+            T temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+        }
+
+        public static int Partition<T>(this IList<T> array, int index, int lenght, T element)
+        {
+            return array.Partition(index, lenght, element, Comparer<T>.Default);
+        }
+
+        public static int Partition<T>(this IList<T> array, int index, int lenght, T element, IComparer<T> comparer)
+        {
+            int i;
+            for (i = index; i < index + lenght - 1; i++)
+            {
+                if (comparer.Compare(array[i], element) == 0)
+                    break;
+            }
+
+            array.Swap(i, index + lenght - 1);
+            i = index;
+            for (int j = index; j <= index + lenght - 2; j++)
+            {
+                if (comparer.Compare(array[j], element) == -1)
+                {
+                    array.Swap(i, j);
+                    i++;
+                }
+            }
+
+            array.Swap(i, index + lenght - 1);
+            return i;
+        }
+
+        public static T KthElement<T>(this IEnumerable<T> data, int k)
+        {
+            var array = data.ToArray();
+            return KthElement(array, 0, array.Length - 1, k, Comparer<T>.Default);
+        }
+
+        public static T KthElement<T>(this IEnumerable<T> data, int k, IComparer<T> comparer)
+        {
+            var array = data.ToArray();
+            return KthElement(array, 0, array.Length - 1, k, comparer);
+        }
+
+        private static T FindMedian<T>(IList<T> data, int index, int lenght, IComparer<T> comparer)
+        {
+            ArrayList.Adapter((IList)data).Sort(index, lenght, (IComparer)comparer);
+            return data[index + (lenght / 2)];
+        }
+
+        private static T KthElement<T>(T[] array, int left, int right, int k, IComparer<T> comparer)
+        {
+            int n = right - left + 1;
+            var medians = new T[(n + 4) / 5];
+            int i;
+            for (i = 0; i < n / 5; i++)
+                medians[i] = FindMedian(array, left + (i * 5), 5, comparer);
+            if (i * 5 < n)
+            {
+                medians[i] = FindMedian(array, left + (i * 5), n % 5, comparer);
+                i++;
+            }
+
+            T medianOfMedians = (i == 1) ? medians[i - 1] :
+                                KthElement(medians, 0, i - 1, i / 2, comparer);
+            int pos = Partition(array, left, n, medianOfMedians);
+
+            if (pos - left == k)
+                return array[pos];
+            if (pos - left > k)
+                return KthElement(array, left, pos - 1, k, comparer);
+            return KthElement(array, pos + 1, right, k - pos + left - 1, comparer);
+        }
+    }
+}
