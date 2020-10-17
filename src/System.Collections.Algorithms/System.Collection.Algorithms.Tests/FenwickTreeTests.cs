6b20249eaa1dac0218ab86943cf6664dd328acc7
Ivan Matantsev <ivmatan@microsoft.com>
2020-10-17 15:03:41 -0700

    Add FenwickTree<TElement,TValue>

--- src/System.Collections.Algorithms/System.Collection.Algorithms.Tests/FenwickTreeTests.cs
+++ src/System.Collections.Algorithms/System.Collection.Algorithms.Tests/FenwickTreeTests.cs
@@ -6,17 +6,43 @@ namespace System.Collections.Algorithms.Tests
     public class FenwickTreeTests
     {
 
+        public static int Add(int a, int b) => a + b;
+        public static int Minus(int a, int b) => a - b;
+
         [Fact]
         public void GivenArrayWhenConstructFenwickTreeThenNoExceptions()
         {
-            var tree = new FenwickTree<int>(new[] { 1, 2, 3, 4 }, (a, b) => a + b, (a, b) => b - a);
+            var tree = new FenwickTree<int>(new[] { 1, 2, 3, 4 }, Add, Minus);
             Assert.NotNull(tree);
+        }
+
+        [Fact]
+        public void GivenNullArgumentsWhenConstructFenwickTreeThenArgumentNullExceptionThrown()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var tree = new FenwickTree<int>(null, null, null);
+            });
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var tree = new FenwickTree<int>(new[] { 1, 2, 3, 4 }, null, null);
+            });
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var tree = new FenwickTree<int>(new[] { 1, 2, 3, 4 }, (a, b) => (a | b), null);
+            });
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var tree = new FenwickTree<int>(new int[0] { }, (a, b) => (a | b), (a, b) => (a | b));
+            });
         }
 
         [Fact]
         public void GivenSumFenwickTreeWhenAskOperationOnIntervalThenReturnCorrectValues()
         {
-            var tree = new FenwickTree<int>(Enumerable.Range(1, 100), (a, b) => a + b, (a, b) => b - a);
+            var tree = new FenwickTree<int>(Enumerable.Range(1, 100), Add, Minus);
             Assert.Equal(1, tree.GetOperationValueOnInterval(0));
             for (int i = 1; i < tree.Count; i++)
                 Assert.Equal((i + 2) * (i + 1) / 2, tree.GetOperationValueOnInterval(i));
@@ -87,6 +113,33 @@ namespace System.Collections.Algorithms.Tests
                 sum += arr[i];
                 Assert.Equal(sum, tree.GetOperationValueOnInterval(i));
             }
+        }
+
+        [Fact]
+        public void GivenRandomSumFenwickTreeWhenEnumerateThenSameAsOriginalCollection()
+        {
+            var rand = new Random();
+            var arr = new uint[64];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = (uint)rand.Next();
+            var tree = new FenwickTree<uint>(arr, (a, b) => a + b, (a, b) => a - b);
+            Assert.Equal(arr, tree);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (uint)rand.Next();
+                tree[i] = arr[i];
+            }
+            Assert.Equal(arr, tree);
+        }
+
+        [Fact]
+        public void GivenFenwickTreeWhenSetOrGetThroughIndexerOutsideOfArrayThenThrowException()
+        {
+            var tree = new FenwickTree<int>(new[] { 1, 2, 3, 4 }, Add, Minus);
+            Assert.Throws<ArgumentOutOfRangeException>(() => tree[-1]);
+            Assert.Throws<ArgumentOutOfRangeException>(() => tree[5]);
+            Assert.Throws<ArgumentOutOfRangeException>(() => tree[-1] = 1);
+            Assert.Throws<ArgumentOutOfRangeException>(() => tree[5] = 1);
         }
     }
 }
--- src/System.Collections.Algorithms/System.Collections.Algorithms/FenwickTree.cs
+++ src/System.Collections.Algorithms/System.Collections.Algorithms/FenwickTree.cs
@@ -4,43 +4,47 @@
     using System.Linq;
 
     /// <summary>
-    /// Data structure to which allow answer question on value of certain operation on interval [0..R] in O(log(n)) time.
+    /// Data structure which allow answer question on value of certain operation on interval [0..R] in O(log(n)) time.
     /// </summary>
-    /// <typeparam name="T">Type of elements.</typeparam>
-    public class FenwickTree<T> : IReadOnlyCollection<T>
+    /// <typeparam name="TElement">Type of elements.</typeparam>
+    /// <typeparam name="TValue">Type on which operation operate.</typeparam>
+    public class FenwickTree<TElement, TValue> : IReadOnlyCollection<TElement>
     {
-        private readonly T[] _data;
-        private readonly T[] _tree;
+        private readonly TElement[] _data;
+        private readonly TValue[] _tree;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="FenwickTree{T}"/> class.
+        /// Initializes a new instance of the <see cref="FenwickTree{TElement, TValue}"/> class.
         /// </summary>
-        /// <param name="data">Data to store in <see cref="FenwickTree{T}"/>.</param>
-        /// <param name="operation">Operation to perform on data in <see cref="FenwickTree{T}"/>.</param>
-        /// <param name="reverseOperation">Reverse operation on <paramref name="reverseOperation"/>.</param>
+        /// <param name="data">Data to store in <see cref="FenwickTree{TElement, TValue}"/>.</param>
+        /// <param name="operation">Operation to perform on data in <see cref="FenwickTree{TElement, TValue}"/>.</param>
+        /// <param name="reverseOperation">Reverse operation on <paramref name="operation"/>.</param>
+        /// <param name="selector">Function to pick <typeparamref name="TValue"/> from <typeparamref name="TValue"/>.</param>
         /// <param name="defaultValue">Default value for operation.</param>
-        public FenwickTree(IEnumerable<T> data, Func<T, T, T> operation, Func<T, T, T> reverseOperation, T defaultValue = default(T))
+        public FenwickTree(
+            IEnumerable<TElement> data,
+            Func<TValue, TValue, TValue> operation,
+            Func<TValue, TValue, TValue> reverseOperation,
+            Func<TElement, TValue> selector,
+            TValue defaultValue = default(TValue))
         {
             if (data is null)
                 throw new ArgumentNullException(nameof(data));
-            if (operation == null)
-                throw new ArgumentNullException(nameof(operation));
-            if (reverseOperation == null)
-                throw new ArgumentNullException(nameof(reverseOperation));
             _data = data.ToArray();
             if (_data.Length == 0)
                 throw new ArgumentException("Collection is empty", nameof(data));
-            _tree = new T[_data.Length];
-            Operation = operation;
-            ReverseOperation = reverseOperation;
+            _tree = new TValue[_data.Length];
+            Operation = operation ?? throw new ArgumentNullException(nameof(operation));
+            ReverseOperation = reverseOperation ?? throw new ArgumentNullException(nameof(reverseOperation));
+            Selector = selector ?? throw new ArgumentNullException(nameof(selector));
             for (int i = 0; i < _data.Length; i++)
                 _tree[i] = defaultValue;
             for (int i = 0; i < _data.Length; i++)
-                Update(i, _data[i]);
+                Update(i, Selector(_data[i]));
         }
 
         /// <summary>
-        /// Gets the number of elements in the <see cref="FenwickTree{T}"/>.
+        /// Gets the number of elements in the <see cref="FenwickTree{TElement, TValue}"/>.
         /// </summary>
         /// <remarks>
         /// Retrieving the value of this property is an O(1) operation.
@@ -48,15 +52,20 @@
         public int Count => _data.Length;
 
         /// <summary>
-        /// Gets operation to apply to elements of <see cref="FenwickTree{T}"/>.
+        /// Gets operation to apply to elements of <see cref="FenwickTree{TElement, TValue}"/>.
         /// </summary>
         /// <remarks>Must be reversable.</remarks>
-        public Func<T, T, T> Operation { get; }
+        public Func<TValue, TValue, TValue> Operation { get; }
 
         /// <summary>
-        /// Gets reverse operation to match <see cref="FenwickTree{T}.Operation"/>.
+        /// Gets reverse operation to match <see cref="FenwickTree{TElement, TValue}.Operation"/>.
         /// </summary>
-        public Func<T, T, T> ReverseOperation { get; }
+        public Func<TValue, TValue, TValue> ReverseOperation { get; }
+
+        /// <summary>
+        /// Gets function which converts <typeparamref name="TElement"/> into <typeparamref name="TValue"/>.
+        /// </summary>
+        public Func<TElement, TValue> Selector { get; }
 
         /// <summary>
         /// Gets or sets the element at the specified index.
@@ -64,11 +73,11 @@
         /// <param name="index">The zero-based index of the element to get or set.</param>
         /// <returns>The element at the specified index.</returns>
         /// <remarks>Set operation is O(log(N).</remarks>
-        public T this[int index]
+        public TElement this[int index]
         {
             get
             {
-                if (index >= _data.Length)
+                if (index >= _data.Length || index < 0)
                 {
                     throw new ArgumentOutOfRangeException(nameof(index));
                 }
@@ -78,26 +87,26 @@
 
             set
             {
-                if (index >= _data.Length)
+                if (index >= _data.Length || index < 0)
                 {
                     throw new ArgumentOutOfRangeException(nameof(index));
                 }
 
-                var inc = ReverseOperation(value, _data[index]);
+                var inc = ReverseOperation(Selector(value), Selector(_data[index]));
                 _data[index] = value;
                 Update(index, inc);
             }
         }
 
         /// <summary>
-        /// Get result of <see cref="FenwickTree{T}.Operation"/> performed on interval [0, pos] in <see cref="FenwickTree{T}"/>.
+        /// Get result of <see cref="FenwickTree{TElement, TValue}.Operation"/> performed on interval [0, pos] in <see cref="FenwickTree{TElement, TValue}"/>.
         /// </summary>
         /// <param name="pos">Right border of interval. Inclusive.</param>
-        /// <returns>Result of <see cref="FenwickTree{T}.Operation"/> applied to all values in positions from 0 to <paramref name="pos"/>.</returns>
+        /// <returns>Result of <see cref="FenwickTree{TElement, TValue}.Operation"/> applied to all values in positions from 0 to <paramref name="pos"/>.</returns>
         /// <remarks>This operation is O(log(N)).</remarks>
-        public T GetOperationValueOnInterval(int pos)
+        public TValue GetOperationValueOnInterval(int pos)
         {
-            T result = _tree[pos];
+            TValue result = _tree[pos];
             pos = (pos & (pos + 1)) - 1;
             while (pos >= 0)
             {
@@ -109,18 +118,18 @@
         }
 
         /// <summary>
-        /// Returns an enumerator that iterates through the <see cref="FenwickTree{T}"/>.
+        /// Returns an enumerator that iterates through the <see cref="FenwickTree{TElement, TValue}"/>.
         /// </summary>
-        /// <returns>An enumerator for the contents of the  <see cref="FenwickTree{T}"/>.</returns>
-        public IEnumerator<T> GetEnumerator() => _data.AsEnumerable().GetEnumerator();
+        /// <returns>An enumerator for the contents of the  <see cref="FenwickTree{TElement, TValue}"/>.</returns>
+        public IEnumerator<TElement> GetEnumerator() => _data.AsEnumerable().GetEnumerator();
 
         /// <summary>
-        /// Returns an enumerator that iterates through the <see cref="FenwickTree{T}"/>.
+        /// Returns an enumerator that iterates through the <see cref="FenwickTree{TElement, TValue}"/>.
         /// </summary>
-        /// <returns>An enumerator for the contents of the  <see cref="FenwickTree{T}"/>.</returns>
+        /// <returns>An enumerator for the contents of the  <see cref="FenwickTree{TElement, TValue}"/>.</returns>
         IEnumerator IEnumerable.GetEnumerator() => _data.GetEnumerator();
 
-        private void Update(int pos, T increment)
+        private void Update(int pos, TValue increment)
         {
             for (; pos < _data.Length; pos = pos | (pos + 1))
                 _tree[pos] = Operation(_tree[pos], increment);
